f32a478eb1e2da74e2b4edb514664ac989dd0694
aipl <YEY0929@163.com>
2020-03-14 22:11:08 +0800

    add mongodb

---
+++ src/main/java/com/henu/demo/config/MongoDBConfig.java
@@ -0,0 +1,33 @@
+package com.henu.demo.config;
+
+import com.mongodb.MongoClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.core.MongoTemplate;
+
+
+/*
+ *MongoDB配置类
+ */
+@Configuration
+public class MongoDBConfig {
+//    public @Bean
+//    MongoClient mongoClient() {
+//        return new MongoClient("192.168.0.103");
+//    }
+//
+//    public @Bean
+//    MongoTemplate mongoTemplate() {
+//        return new MongoTemplate(mongoClient(), "mytest");
+//    }
+
+    /*
+     * Factory bean that creates the com.mongodb.MongoClient instance
+     */
+//    public @Bean
+//    MongoClientFactoryBean mongo() {
+//        MongoClientFactoryBean mongo = new MongoClientFactoryBean();
+//        mongo.setHost("192.168.0.103");
+//        return mongo;
+//    }
+}
--- src/main/java/com/henu/demo/controller/UserController.java
+++ src/main/java/com/henu/demo/controller/UserController.java
@@ -5,10 +5,13 @@ import com.henu.demo.entity.Area;
 import com.henu.demo.entity.Message;
 import com.henu.demo.service.KafkaSendService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.BasicQuery;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
@@ -49,8 +52,20 @@ public class UserController {
         }
         return "success";
     }
+
+    @Autowired
+    private MongoTemplate mongoTemplate;
     @GetMapping("/hello")
-    public String hello(){
-        return "hello";
+    @ResponseBody
+    public String test() {
+        List<Area> areaList = new ArrayList<>();
+        //使用基础查询可以直接使用mongo的语句
+        //这个查询在区间为小于5和大于995的文档，如果出现java语句不能表述，可以使用基本查询
+        BasicQuery query = new BasicQuery("{$or:[{\"areaId\":{$lt:5}},{\"areaId\":{$gt:995}}]}");
+        areaList = mongoTemplate.find(query,Area.class,"area");
+        for (Object area : areaList) {
+            System.out.println(area.toString());
+        }
+        return "success";
     }
 }
